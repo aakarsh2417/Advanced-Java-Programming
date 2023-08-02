3751a806cc5a55cd37dd507fa7e712047ae42d5f
Aakarsh Sharma <115183827+aakarshs817@users.noreply.github.com>
2023-08-02 23:45:26 +0530

    Creating Database using JDBC

---
+++ JDBC.java
@@ -0,0 +1,22 @@
+import java.sql.*;
+public class DB
+{
+static final String DB_URL = "jdbc:mysql://localhost:3306/?user=root";
+static final String USER = "root";
+static final String PSWD = "abcd1234";
+public static void main (String[] args) throws Exception
+{
+Connection con = null;
+Statement stmt = null;
+Class.forName("com.mysql.jdbc.Driver");
+System.out.println("Connecting to Database");
+con = DriverManager.getConnection(DB_URL,USER,PSWD);
+System.out.println("Creating Database");
+stmt = con.createStatement();
+String sql = "CREATE DATABASE TESTING";
+stmt.executeUpdate(sql);
+System.out.println("Database Created Successfully");
+con.close();
+stmt.close();
+}
+}
